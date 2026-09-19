1b0713f488d243cc6fe000b13afa18d9811a7f4a
Lance0174 <yan3345174261@163.com>
2026-09-20 00:20:07 +0800

    build(android): bump version to 1.8.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.akudamatata.solara"
         minSdk = 29
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.8.1"
+        versionCode = 12
+        versionName = "1.8.2"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
